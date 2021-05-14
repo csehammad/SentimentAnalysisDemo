540328258449b25c65124099bf21a74e80900db8
Hammad Ul Haq <cse.hammad@gmail.com>
2021-05-15 00:03:00 +0500

    SentimentAnalysis Demo using ML.NET in .NET5

---
+++ SentimentAnalysisDemo/Controllers/SentimentAnalysisController.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SentimentAnalysisDemoML.Model;
+
+namespace SentimentAnalysisDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SentimentAnalysis : Controller
+    { 
+ 
+
+        [HttpGet]
+        public JsonResult  AnalyzeFeedback(string feedback)
+        {
+            try
+            {
+                ModelInput _data = new();
+
+                _data.Col0 = feedback;
+
+                var predictionResult = ConsumeModel.Predict(_data);
+
+                return Json(new
+                {
+
+                    Prediction = predictionResult.Prediction,
+                    Score = predictionResult.Score
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                  
+                    Error="Unable to Consume Model"
+                });
+            }
+
+        }
+    }
+}
